c03f96e487983b6749518a9c26edd3896b0a41ba
Allen Mikhailov <slinkyshelf8@gmail.com>
2024-11-12 17:30:47 -0600

    Lots of updates made to the code

--- bio-heating-controller/run_expiriment.js
+++ bio-heating-controller/run_expiriment.js
@@ -2,25 +2,73 @@ import { Timestamp } from 'firebase/firestore';
 import { readFileSync, readdirSync } from 'fs';
 import { db } from './firebase.js';
 import { addDoc, collection } from 'firebase/firestore';
+import { createTransport } from 'nodemailer';
 
 // Getting File path
 const devices_path = "/sys/bus/w1/devices/w1_bus_master1/"
 
-const CONTROL_DEVICE = "28-3cd8f64961fe"
-const EXPIRIMENTAL_DEVICE = "28-01144d041caa"
+const CONTROL_DEVICE = "28-3cf104575517"
+const EXPERIMENTAL_DEVICE = "28-3cd8f64961fe"
 
 const EXPERIMENT_NAME = "test1"
 const READ_INTERVAL = 3 * 1000
 const PACKET_LENGTH = 10
 
+const EMAIL = "bioheating.rice@gmail.com"
+const EMAIL_TARGET = "slinkyshelf8@gmail.com"
+
+const password_file = path.resolve(path.dirname(fileURLToPath(import.meta.url)), './email_password.txt')
+
+const transporter = createTransport({
+    service: 'gmail',
+    auth: {
+        user: EMAIL,
+        pass: readFileSync(password_file, "utf-8")
+    }
+});
+
+function send_email(subject, text)
+{
+    transporter.sendMail({
+        from: EMAIL,
+        to: EMAIL_TARGET,
+        subject: subject,
+        text: text
+    }, function(error, info){
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Email sent: ' + info.response);
+        }
+    })
+}
+
+// If sensor outputs this temperature it has failed
+const ERROR_TEMPERATURE = 85
+
+// Creating 
+
+console.log("Finding Sensors:")
 const files = readdirSync(devices_path, { withFileTypes: true })
 const valid_dirents = {}
 files.map(dirent => {
     if (dirent.isDirectory() && dirent.name.startsWith("28"))
+    {
         valid_dirents[dirent.name] = dirent
+        console.log("Found Valid Sensor with ID: \"%s\"", dirent.name)
+    }
 })
 
-console.log(valid_dirents)
+// Checking for Control and Experimental Sensors
+if (valid_dirents[CONTROL_DEVICE] == undefined)
+    console.log("Found Control Sensor")
+else
+    console.error("Unable to find Control Sensor")
+
+if (valid_dirents[EXPERIMENTAL_DEVICE] == undefined)
+    console.log("Found Experiment Sensor")
+else
+    console.error("Unable to find Experiment Sensor")
 
 function sleep(ms) {
     return new Promise((resolve) => {
@@ -46,13 +94,22 @@ function generate_new_packet()
 
 let packet = generate_new_packet()
 let packet_additions = 0
+let total_packets_sent = 0
 
+send_email("Experiment Online", `The Experiment "${EXPERIMENT_NAME}" is online and running smoothly`)
+
+// Reading Loop
 while (true)
 {
     // Reading Sensors
     const control_temp = read_sensor(valid_dirents[CONTROL_DEVICE])
-    const experimental_temp = read_sensor(valid_dirents[EXPIRIMENTAL_DEVICE])
+    const experimental_temp = read_sensor(valid_dirents[EXPERIMENTAL_DEVICE])
 
+    if (control_temp == ERROR_TEMPERATURE)
+        console.error("Control Sensor is erroring")
+
+    if (experimental_temp == ERROR_TEMPERATURE)
+        console.error("Experiment Sensor is erroring")
 
     // Logging
     console.log("Control %s C, Expirment %s C, dif %s", 
@@ -66,7 +123,8 @@ while (true)
     packet.experimental_temp.push(experimental_temp)
     packet.temperature_timestamps.push(Timestamp.now())
 
-    packet_additions += 1
+    packet_additions++;
+    total_packets_sent++;
 
     // Sending Data to server
     if (packet_additions >= PACKET_LENGTH)
@@ -75,6 +133,7 @@ while (true)
         addDoc(collection(db, "experiment_data"), packet)
         packet = generate_new_packet()
         packet_additions = 0
+        console.log("Sent packet #%s to server", total_packets_sent)
     }
 
 
