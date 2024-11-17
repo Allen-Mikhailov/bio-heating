4df108d82b4c5f6cbbd14128a0f6310f327ebe81
Allen Mikhailov <slinkyshelf8@gmail.com>
2024-11-16 20:48:38 -0600

    test pt5

--- bio-heating-controller/firebase.js
+++ bio-heating-controller/firebase.js
@@ -3,7 +3,7 @@ import { fileURLToPath } from 'url';
 
 import { config } from 'dotenv';
 config({path: path.dirname(fileURLToPath(import.meta.url))+"/.env"})
-console.log("firebase.js", import.meta.dirname+"/.env", path.dirname())
+console.log("firebase.js", path.dirname(fileURLToPath(import.meta.url))+"/.env")
 
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
