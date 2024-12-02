c7577136c2f2cd9175c426f0ba9258bbfb75eb6f
Allen Mikhailov <slinkyshelf8@gmail.com>
2024-12-01 21:37:45 -0600

    fixed

--- bio-heating-controller/src/action_server.ts
+++ bio-heating-controller/src/action_server.ts
@@ -63,7 +63,7 @@ class ActionServer
     async start()
     {   
         const server = http.createServer((req, res) => {
-            res.setHeader('Access-Control-Allow-Origin', `http://localhost:${this.port}`); // Allow your frontend
+            res.setHeader('Access-Control-Allow-Origin', `*`); // Allow your frontend
             res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS'); // Allow specific methods
             res.setHeader('Access-Control-Allow-Headers', 'Content-Type'); // Allow specific headers
             if (req.method == "POST")
