d183290476905b936cd61c218f136a9e40aac88d
Allen Mikhailov <slinkyshelf8@gmail.com>
2024-12-09 22:57:20 -0600

    test2

--- bio-heating-controller/src/index.ts
+++ bio-heating-controller/src/index.ts
@@ -204,7 +204,7 @@ async function start_device() {
         startup_fail(["write_device_opening_to_firebase"])
     }
 
-    send_email("Device Online", `The Device "${env.DEVICE_ID}" is online and running smoothly v1.1`)
+    send_email("Device Online", `The Device "${env.DEVICE_ID}" is online and running smoothly v1.2`)
 }
 
 start_device();
