74d2cb94b635a5290316c5c1cbb66e067fe17e36
Allen Mikhailov <slinkyshelf8@gmail.com>
2025-01-30 15:09:47 -0600

    Might fix builds

---
+++ bio-heating-controller/src/shared/src/interfaces.d.ts
@@ -0,0 +1,51 @@
+import { Timestamp } from "firebase/firestore";
+interface SensorConfig {
+    id: string;
+    calibration: number;
+    optional: boolean;
+}
+interface DeviceState {
+    runningExperiment: boolean;
+    runningExperimentId: string;
+    runningExperimentType: string;
+}
+interface DeviceData {
+    active_url: string;
+    device_id: string;
+    ip_address: string;
+    last_activity: Timestamp;
+    server_up_time: Timestamp;
+    is_active: boolean;
+    version: string;
+}
+interface DevicePacket {
+    all_device_sensors: string[];
+    sensor_config: {
+        [key: string]: SensorConfig;
+    };
+    simulation_sensor_configs: {
+        [key: string]: string;
+    };
+    env: {
+        [key: string]: string;
+    };
+    sensor_readings: {
+        [key: string]: number;
+    };
+    device_state: DeviceState;
+    memory_usage: number;
+    cpu_temperature: number;
+}
+interface DeviceMark {
+    deviceId: string;
+    startTime: Timestamp;
+}
+interface ExperimentData {
+    creationDate: Timestamp;
+    experimentType: string;
+    marks: DeviceMark[];
+}
+type ExperimentMap = {
+    [key: string]: ExperimentData;
+};
+export type { SensorConfig, DeviceData, DevicePacket, DeviceMark, ExperimentData, ExperimentMap, DeviceState };
